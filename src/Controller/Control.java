02e427f15f0b3aebe4febf9d0c430f9568472af3
Libba Lawrence <ll313@duke.edu>
2020-02-19 21:28:09 -0500

    linking up controller

---
+++ src/Controller/Control.java
@@ -0,0 +1,54 @@
+package Controller;
+
+import backEnd.ErrorHandler;
+import backEnd.PenUpdate;
+import backEnd.TurtleUpdate;
+import frontEnd.View;
+import java.util.Arrays;
+import java.util.List;
+import slogo.Main;
+
+public class Control {
+
+  private static final String WHITESPACE = " ";
+  private View view;
+  private PenUpdate pen;
+  private TurtleUpdate turtle;
+  private ErrorHandler error;
+  private Parser parser;
+
+  public Control(String command){
+    view = new View();
+    pen = new PenUpdate();
+    turtle = new TurtleUpdate();
+    error = new ErrorHandler();
+    parser = new Parser();
+  }
+
+
+  public void parseCommand(){
+    Control m = new Control(view.display());
+    // these are more specific, so add them first to ensure they are checked first
+    parser.addPatterns("English");
+    // general checks, added last
+    parser.addPatterns("Syntax");
+
+    // try against different kinds of inputs
+    m.parseText(parser, view.display());
+    String userInput = "fd 50 rt 90 BACK :distance Left :angle";
+    // note, this simple "algorithm" will not handle SLogo comments
+  }
+
+
+  // given some text, prints results of parsing it using the given language
+  private void parseText (Parser parser, String lines) {
+    for (String line : lines.split(WHITESPACE)) {
+      if (line.trim().length() > 0) {
+        System.out.println(String.format("%s : %s", line, parser.getSymbol(line)));
+      }
+    }
+    System.out.println();
+  }
+
+
+}
--- src/Controller/Parser.java
+++ src/Controller/Parser.java
@@ -10,7 +10,7 @@ import java.util.regex.Pattern;
 
 public class Parser {
   // where to find resources specifically for this class
-  private static final String RESOURCES_PACKAGE = Parser.class.getPackageName() + ".resources.languages.";
+  private static final String RESOURCES_PACKAGE = "resources.languages.";
   // "types" and the regular expression patterns that recognize those types
   // note, it is a list because order matters (some patterns may be more generic)
   private List<Entry<String, Pattern>> mySymbols;
