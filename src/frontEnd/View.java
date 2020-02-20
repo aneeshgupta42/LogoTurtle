9205e67cdd0f7208b91b165852136ed49f7ed9db
CAYLA SCHUVAL <cas169@duke.edu>
2020-02-20 10:43:20 -0500

    added methods to get text to back end and ended buttons

--- src/frontEnd/View.java
+++ src/frontEnd/View.java
@@ -1,17 +1,21 @@
 package frontEnd;
 
-import java.awt.TextArea;
 import java.util.Map;
 import java.util.ResourceBundle;
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToolBar;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import frontEnd.View;
@@ -55,7 +59,9 @@ public class View extends Application {
   public static final String TITLE = "JavaFX Animation Example";
   public static final Dimension DEFAULT_SIZE = new Dimension(1600, 1600);
   private Node myActor;
-  private static final String COURT = "";
+  private TextArea myCommander;
+  private static final String TURTLE = "turtle.png";
+  private String myText;
 
   public View() {
    // myTurtle =
@@ -87,36 +93,25 @@ public class View extends Application {
     myStage.show();
     myStage.setOnCloseRequest(t->stopEverything());
     Animation animation = makeAnimation(myActor);
-    animation.play();
+    //animation.play();
   }
 
   /**
    * Returns scene for the browser so it can be added to stage.
    */
-  /*public Scene makeScene(int width, int height) {
 
-    Scene scene = new Scene(root, width, height);
-    return scene;
-  }*/
-
-  // create a simple scene
   private Scene makeScene (int width, int height) {
-    //Group root = new Group();
     BorderPane root = new BorderPane();
-    root.setTop(makeDisplayWindow());
-    root.setLeft(makeDisplayWindow());
-    root.setRight(makeDisplayWindow());
+    HBox hbox = addHBox();
+    root.setTop(hbox);
     root.setCenter(makeDisplayWindow());
+    root.setRight(makeSideWindow());
+    //root.setCenter(makeDisplayWindow());
+    //myCommander = makeCommandWindow();
     root.setBottom(makeCommandWindow());
-    Image court= new Image(this.getClass().getClassLoader().getResourceAsStream(TURTLE));
-    ImageView myTurtle = new ImageView(court);
+    //root.getChildren().add(myTurtle);
     myActor = makeActor();
-    root.getChildren().add(myActor);
-
-    //root.getChildren().add(makeDisplayWindow());
-    //root.getChildren().add(makeCommandWindow());
-    //TextField inputArea = new TextField();
-    //root.getChildren().add(inputArea);
+    //root.getChildren().add(myActor);
     return new Scene(root, width,  height);
   }
 
@@ -133,26 +128,71 @@ public class View extends Application {
 
   private Node makeDisplayWindow(){
     GridPane gridPane = new GridPane();
-    gridPane.setMinSize(1600, 400);
+    //gridPane.setMaxSize(1200, 400);
     gridPane.setStyle("-fx-padding: 10;" +
         "-fx-border-style: solid inside;" +
         "-fx-border-width: 2;" +
         "-fx-border-insets: 5;" +
         "-fx-border-radius: 5;" +
-        "-fx-border-color: blue;");
+        "-fx-border-color: grey;");
+    Image turtle= new Image(getClass().getClassLoader().getResourceAsStream(TURTLE));
+    ImageView myTurtle = new ImageView(turtle);
+    myTurtle.setY(gridPane.getBoundsInLocal().getHeight()/2);
+    myTurtle.setX(gridPane.getBoundsInLocal().getWidth()/2);
+    gridPane.getChildren().add(myTurtle);
     return gridPane;
   }
+
+
+  private Node makeSideWindow() {
+    GridPane gridPane = new GridPane();
+    gridPane.setMinHeight(800);
+    gridPane.setMinWidth(400);
+    gridPane.setStyle("-fx-padding: 10;" +
+        "-fx-border-style: solid inside;" +
+        "-fx-border-width: 2;" +
+        "-fx-border-insets: 5;" +
+        "-fx-border-radius: 5;" +
+        "-fx-border-color: grey;");
+    return gridPane;
+  }
+
   private Node makeCommandWindow(){
-    TextField inputArea = new TextField();
+    HBox hbox = new HBox();
+    TextArea inputArea = new TextArea();
     inputArea.setPromptText("Enter Command");
     inputArea.setPrefColumnCount(10);
     inputArea.getText();
     GridPane.setConstraints(inputArea, 0, 0);
     //inputArea.getText();
     //inputArea.clear();
-    inputArea.setMinSize(1600, 200);
-    inputArea.setStyle("-fx-border-color: red;");
-    return inputArea;
+    inputArea.setMinHeight(200);
+    inputArea.setMinWidth(1500);
+    inputArea.setStyle("-fx-padding: 10;" +
+        "-fx-border-style: solid inside;" +
+        "-fx-border-width: 2;" +
+        "-fx-border-insets: 5;" +
+        "-fx-border-radius: 5;" +
+        "-fx-border-color: #336699;");
+    VBox vbox = new VBox();
+    Button runButton = new Button("Run");
+    runButton.setPrefSize(100, 20);
+
+    runButton.setOnAction(action -> {
+      myText = inputArea.getText();
+      System.out.println(myText);
+      inputArea.setText("Running!");
+    });
+    Button clearButton = new Button("Clear");
+    clearButton.setPrefSize(100, 20);
+
+    clearButton.setOnAction(action -> {
+      inputArea.setText("");
+    });
+    vbox.getChildren().addAll(runButton, clearButton);
+    hbox.getChildren().addAll(inputArea, vbox);
+
+    return hbox;
   }
 
   private void stopEverything(){
@@ -172,8 +212,28 @@ public class View extends Application {
     return new SequentialTransition(agent, pt, rt);
   }
 
+  public HBox addHBox() {
+    HBox hbox = new HBox();
+    hbox.setPadding(new Insets(15, 12, 15, 12));
+    hbox.setSpacing(10);
+    hbox.setStyle("-fx-background-color: #336699");
+
+    Button buttonCurrent = new Button("Current");
+    buttonCurrent.setPrefSize(100, 20);
+
+    Button buttonProjected = new Button("Projected");
+    buttonProjected.setPrefSize(100, 20);
+
+    hbox.getChildren().addAll(buttonCurrent, buttonProjected);
+
+    return hbox;
+  }
+
+  public String getText(){
+    return myText;
+  }
+
   public static void main(String[] args) {
     launch(args);
   }
-
 }
