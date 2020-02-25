8f5062560a0b7339d27160fd64933110b6f3de2a
Turner Jordan <tgj5@duke.edu>
2020-02-25 17:15:02 -0500

    added functionality for NotEqual and Equal command classes

--- src/backEnd/commands/Boolean/NotEqual.java
+++ src/backEnd/commands/Boolean/NotEqual.java
@@ -5,4 +5,41 @@ import backEnd.commands.Command;
 import java.util.LinkedList;
 
 public class NotEqual extends Command {
+    private static final int NUMARGS = 2;
+    private static final double TRUE = 1.0;
+    private static final double FALSE = 0.0;
+    private double booleanResult;
+
+    public NotEqual() {
+        super();
+        super.numberOfArgs = NUMARGS;
+    }
+
+    public NotEqual(LinkedList<String> varargs, Control control) {
+        super(varargs, control);
+        super.numberOfArgs = NUMARGS;
+
+        double argOne = Double.parseDouble(varargs.get(0));
+        double argTwo = Double.parseDouble(varargs.get(1));
+
+        if(argOne != argTwo) {
+            booleanResult = TRUE;
+        } else {
+            booleanResult = FALSE;
+        }
+
+        System.out.println("The notEquals result is: " + booleanResult);
+    }
+
+    @Override
+    public String commandValueReturn() {
+        String ret = Double.toString(booleanResult);
+        return ret;
+    }
+
+    @Override
+    public int repeatCom() {
+        return super.repeatCom();
+    }
 }
+
