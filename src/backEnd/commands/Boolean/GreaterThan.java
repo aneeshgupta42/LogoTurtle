dc8602e7ebf3d3581798be3c0ac894b1e09477ca
Turner Jordan <tgj5@duke.edu>
2020-02-25 17:03:19 -0500

    added GreaterThan command class functionality

--- src/backEnd/commands/Boolean/GreaterThan.java
+++ src/backEnd/commands/Boolean/GreaterThan.java
@@ -5,4 +5,40 @@ import backEnd.commands.Command;
 import java.util.LinkedList;
 
 public class GreaterThan extends Command {
+    private static final int NUMARGS = 2;
+    private static final double TRUE = 1.0;
+    private static final double FALSE = 0.0;
+    private double booleanResult;
+
+    public GreaterThan() {
+        super();
+        super.numberOfArgs = NUMARGS;
+    }
+
+    public GreaterThan(LinkedList<String> varargs, Control control) {
+        super(varargs, control);
+        super.numberOfArgs = NUMARGS;
+
+        double argOne = Double.parseDouble(varargs.get(0));
+        double argTwo = Double.parseDouble(varargs.get(1));
+
+        if(argOne > argTwo) {
+            booleanResult = TRUE;
+        } else {
+            booleanResult = FALSE;
+        }
+
+        System.out.println("The GreaterThan result is: " + booleanResult);
+    }
+
+    @Override
+    public String commandValueReturn() {
+        String ret = Double.toString(booleanResult);
+        return ret;
+    }
+
+    @Override
+    public int repeatCom() {
+        return super.repeatCom();
+    }
 }
