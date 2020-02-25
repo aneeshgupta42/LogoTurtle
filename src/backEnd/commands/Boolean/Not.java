b212fe0cf377b8537f09c279ed76d61e434d2b6c
Turner Jordan <tgj5@duke.edu>
2020-02-25 17:28:03 -0500

    added functionality to Not command classes -- all Boolean command classes done

--- src/backEnd/commands/Boolean/Not.java
+++ src/backEnd/commands/Boolean/Not.java
@@ -5,4 +5,37 @@ import backEnd.commands.Command;
 import java.util.LinkedList;
 
 public class Not extends Command {
+    private static final int NUMARGS = 1;
+    private static final double TRUE = 1.0;
+    private static final double FALSE = 0.0;
+    private double booleanResult;
+
+    public Not() {
+        super();
+        super.numberOfArgs = NUMARGS;
+    }
+
+    public Not(LinkedList<String> varargs, Control control) {
+        super(varargs, control);
+        super.numberOfArgs = NUMARGS;
+
+        double argOne = Double.parseDouble(varargs.get(0));
+        if(argOne == 0) {
+            booleanResult = TRUE;
+        } else {
+            booleanResult = FALSE;
+        }
+
+        System.out.println("The Not result is: " + booleanResult);
+    }
+
+    @Override
+    public String commandValueReturn() {
+        return Double.toString(booleanResult);
+    }
+
+    @Override
+    public int repeatCom() {
+        return super.repeatCom();
+    }
 }
