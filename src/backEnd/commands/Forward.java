f107d2131778da8f8b9d18ebaf413c823196b804
Aneesh Gupta <ag468@duke.edu>
2020-02-25 18:16:32 -0500

    working more on commands

--- src/backEnd/commands/Forward.java
+++ src/backEnd/commands/Forward.java
@@ -5,24 +5,31 @@ import java.util.LinkedList;
 
 public class Forward extends Command {
   private static final int NUMARGS = 1;
+  private int distance;
 
   public Forward(){
     super();
     super.numberOfArgs=NUMARGS;
   }
+
   public Forward(LinkedList<String> varargs, Control control){
     super(varargs, control);
     System.out.println("Reached");
     int i=0;
     i++;
     System.out.println(i);
-    int distance = (int)(Double.parseDouble(varargs.get(0)));
+    distance = (int)(Double.parseDouble(varargs.get(0)));
     double angle = control.getTurtleAngle();
 //    System.out.println("Angle: " + angle);
     double newX = (distance *(Math.sin(Math.toRadians((angle)))));
     double newY = -(distance *(Math.cos(Math.toRadians((angle)))));
     System.out.println("d " + distance);
     control.updateTurtle(newX, newY, 0, distance);
+  }
+
+  @Override
+  public String commandValueReturn() {
+    return Integer.toString(distance);
   }
 
   @Override
