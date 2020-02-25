4de5c260967d4531be20bde3d34fd35eab755583
Turner Jordan <tgj5@duke.edu>
2020-02-25 05:10:29 -0500

    Power and Random command classes implemented

--- src/backEnd/commands/Math/Power.java
+++ src/backEnd/commands/Math/Power.java
@@ -3,6 +3,31 @@ package backEnd.commands.Math;
 import Controller.Control;
 import backEnd.commands.Command;
 import java.util.LinkedList;
+import java.lang.Math;
 
-public class Power extends Command{
+public class Power extends Command {
+    private static final int NUMARGS = 2;
+    private double result;
+
+    public Power() {
+        super();
+        super.numberOfArgs = NUMARGS;
+    }
+
+    public Power(LinkedList<String> varargs, Control control) {
+        super(varargs, control);
+        super.numberOfArgs = NUMARGS;
+
+        double power = Double.parseDouble(varargs.get(0));
+        double base = Double.parseDouble(varargs.get(1));
+        result = Math.pow(base, power);
+
+        System.out.println("The exponential result is: " + result);
+    }
+
+    @Override
+    public String commandValueReturn() {
+        String ret = Double.toString(result);
+        return ret;
+    }
 }
--- src/backEnd/commands/Math/Random.java
+++ src/backEnd/commands/Math/Random.java
@@ -3,6 +3,31 @@ package backEnd.commands.Math;
 import Controller.Control;
 import backEnd.commands.Command;
 import java.util.LinkedList;
+import java.lang.Math;
 
-public class Random extends Command{
+public class Random extends Command {
+    private static final int NUMARGS = 1;
+    private double random;
+
+    public Random() {
+        super();
+        super.numberOfArgs = NUMARGS;
+    }
+
+    public Random(LinkedList<String> varargs, Control control) {
+        super(varargs, control);
+        super.numberOfArgs = NUMARGS;
+
+        double max = Double.parseDouble(varargs.get(0));
+        random = Math.random() * max;
+        // insert check if max is too big ?
+
+        System.out.println("The random number is: " + random);
+    }
+
+    @Override
+    public String commandValueReturn() {
+        String ret = Double.toString(random);
+        return ret;
+    }
 }
