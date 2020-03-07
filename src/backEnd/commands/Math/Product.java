d562cfbb805fee838bd9a24d478f171a7e6a4030
Libba Lawrence <ll313@duke.edu>
2020-03-07 02:27:02 -0500

    fixed switch var

--- src/backEnd/commands/Math/Product.java
+++ src/backEnd/commands/Math/Product.java
@@ -17,8 +17,8 @@ public class Product extends Command {
         super(varargs, control);
         super.numberOfArgs = NUMARGS;
 
-        double argOne = Double.parseDouble(varargs.get(0));
-        double argTwo = Double.parseDouble(varargs.get(1));
+        double argOne = Double.parseDouble(varargs.get(1));
+        double argTwo = Double.parseDouble(varargs.get(0));
         product = argTwo * argOne;
 
     }
