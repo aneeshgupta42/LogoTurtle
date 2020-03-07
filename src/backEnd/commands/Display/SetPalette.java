1ad1a4db8fbcc32d1c994f6875260519aeab2cda
Turner Jordan <tgj5@duke.edu>
2020-03-07 00:19:32 -0500

    added calls to front end methods in display command controller methods

--- src/backEnd/commands/Display/SetPalette.java
+++ src/backEnd/commands/Display/SetPalette.java
@@ -7,7 +7,7 @@ import java.util.List;
 
 public class SetPalette extends Command {
     private static final int NUMARGS = 4;
-    private int index, red, green, blue;
+    private int userIndex, index, red, green, blue;
 
     public SetPalette() {
         super();
@@ -18,17 +18,18 @@ public class SetPalette extends Command {
         super(varargs, control);
         super.numberOfArgs = NUMARGS;
 
-        index = Integer.parseInt(varargs.get(0));
-        red = Integer.parseInt(varargs.get(1));
-        blue = Integer.parseInt(varargs.get(2));
-        green = Integer.parseInt(varargs.get(3));
+        userIndex = Integer.parseInt(varargs.get(3));
+        index = userIndex - 1;
+        red = Integer.parseInt(varargs.get(2));
+        blue = Integer.parseInt(varargs.get(1));
+        green = Integer.parseInt(varargs.get(0));
 
         control.switchPaletteColor(index, red, green, blue);
     }
 
     @Override
     public String commandValueReturn() {
-        String ret = Integer.toString(index);
+        String ret = Integer.toString(userIndex);
         return ret;
     }
 
